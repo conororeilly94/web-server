25cea321b21ab1fd49b992499c96e0b3de3b546d
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-01 16:54:40 +0000

    added favicon and other features

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 # Home route
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+	return render_template('index.html', name=username, post_id=post_id)
 
 @app.route('/about.html')
 def about():
