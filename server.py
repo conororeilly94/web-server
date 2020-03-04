3b004d2d8146cbf3c0c719795a26dbd5bb7c9d40
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-04 20:25:13 +0000

    writing to file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, csv
 app = Flask(__name__)
 
 # Home route
@@ -10,11 +10,25 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
     	data = request.form.to_dict()
-    	print(data)
+    	write_to_file(data)
     	return redirect('/thankyou.html')
     else:
     	return 'Try again'    
